7cb17d36777083ef7f986f8089971c86267beec1
CandyyPL <aplay58.yt@gmail.com>
2026-03-03 17:00:42 +0100

    refactor responsive view

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -6,37 +6,39 @@ export default function About() {
   return (
     <section
       id={SECTIONS.ABOUT}
-      className='flex flex-col items-center gap-4'>
-      <h1 className='section-heading text-center'>
-        Kilka słów <span className='text-yellow'>o mnie</span>..
+      className='flex w-full flex-col items-center gap-4 md:gap-8 lg:gap-12'>
+      <h1 className='section-heading text-center md:w-full md:pr-10 md:text-end'>
+        Kilka słów <span className='text-yellow'>o mnie</span>.
       </h1>
-      <img
-        src='/profile2.webp'
-        alt='photo'
-        className='border-yellow-dim rounded-md border-4'
-      />
-      <div className='bg-background-light relative rounded-sm p-4 opacity-80'>
+      <div className='flex w-full flex-col items-center gap-8 md:flex-row md:justify-center'>
         <img
-          src={quoteIcon}
-          alt='quote'
-          className='absolute top-0 right-0 z-0 size-24 opacity-20'
+          src='/profile2.webp'
+          alt='photo'
+          className='border-yellow-dim max-h-120 rounded-md border-4'
         />
-        <p className='text-light-dim font-poppins relative z-1 mb-2 text-sm font-medium text-balance shadow-xs'>
-          Wybierz pracę, którą kochasz, a nie przepracujesz ani jednego dnia w
-          swoim życiu.
-        </p>
-        <p className='text-light-dim font-poppins w-full text-end text-sm'>
-          Konfucjusz
-        </p>
+        <div className='flex w-fit flex-col items-center gap-8'>
+          <div className='bg-background-light text-light-dim font-poppins relative rounded-sm p-4 text-sm opacity-80 md:max-w-150 md:text-lg xl:max-w-175 xl:text-xl'>
+            <img
+              src={quoteIcon}
+              alt='quote'
+              className='absolute top-0 right-0 z-0 size-24 opacity-20'
+            />
+            <p className='relative z-1 mb-2 w-2/3 font-medium text-balance'>
+              Wybierz pracę, którą kochasz, a nie przepracujesz ani jednego dnia
+              w swoim życiu.
+            </p>
+            <p className='w-full text-end'>Konfucjusz</p>
+          </div>
+          <p className='text-light px-4 text-center text-lg leading-tight md:max-w-150 md:text-xl xl:max-w-175 xl:text-xl'>
+            W pełni zgadzam się z tymi słowami. Programowanie to moja pasja,
+            którą rozwijam od wielu lat, dlatego postanowiłem uczynić z tego
+            swoją ścieżkę zawodową. <br /> <br /> Kodowanie to nie tylko moje
+            hobby, ale również styl życia, który daje mi niesamowitą satysfakcję
+            i nieustannie inspiruje do rozwoju, a w niedługiej przyszłości
+            stanie się również moją pracą.
+          </p>
+        </div>
       </div>
-      <p className='text-light px-4 text-center text-lg leading-tight'>
-        W pełni zgadzam się z tymi słowami. Programowanie to moja pasja, którą
-        rozwijam od wielu lat, dlatego postanowiłem uczynić z tego swoją ścieżkę
-        zawodową. <br /> <br /> Kodowanie to nie tylko moje hobby, ale również
-        styl życia, który daje mi niesamowitą satysfakcję i nieustannie
-        inspiruje do rozwoju, a w niedługiej przyszłości stanie się również moją
-        pracą.
-      </p>
       <TechStack />
     </section>
   );
