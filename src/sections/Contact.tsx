6e04a532ea6cfe0ed5ce27ed842c4cd9b3e278ea
CandyyPL <aplay58.yt@gmail.com>
2026-03-04 12:33:53 +0100

    refactor styles of contact section

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -29,51 +29,49 @@ export default function Contact() {
   return (
     <section
       id={SECTIONS.CONTACT}
-      className='mb-4 flex w-full flex-col items-center gap-4 md:mb-8 md:gap-8 xl:mb-12 xl:gap-12'>
-      <h1 className='section-heading text-center md:w-full md:text-start'>
+      className='flex w-full flex-col items-center gap-12 pt-5 xl:mb-25'>
+      <h1 className='section-heading text-center xl:text-left'>
         <span className='text-yellow'>Skontaktuj się</span> ze mną.
       </h1>
       <div className='flex w-full flex-col items-center justify-center gap-20 lg:flex-row'>
         <img
           src={emailIcon}
           alt='mail'
-          className='max-h-100'
+          className='max-w-100'
         />
-        <div className='bg-light-dim w-full max-w-150'>
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            className='flex h-full w-full flex-col items-center gap-4 p-4'>
-            <div className='field-wrapper'>
-              <label
-                htmlFor='email'
-                className='ml-1'>
-                E-mail
-              </label>
-              <input
-                type='text'
-                id='email'
-                className='form-input'
-                {...register('email')}
-              />
-              {errors.email && (
-                <p className='input-error'>Niepoprawny adres e-mail.</p>
-              )}
-            </div>
-            <div className='field-wrapper'>
-              <label htmlFor='message'>Wiadomość</label>
-              <textarea
-                id='message'
-                className='form-input h-50 resize-none'
-                {...register('message')}></textarea>
-              {errors.message && (
-                <p className='input-error'>Wpisz pomiędzy 20 a 400 znaków.</p>
-              )}
-            </div>
-            <button className='bg-yellow hover:bg-yellow-dim transition-bg h-14 w-30 cursor-pointer text-xl font-medium duration-200'>
-              WYŚLIJ
-            </button>
-          </form>
-        </div>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className='bg-light-dim flex w-full max-w-131.25 flex-col items-center gap-7 p-4'>
+          <div className='field-wrapper'>
+            <label
+              htmlFor='email'
+              className='ml-1'>
+              E-mail
+            </label>
+            <input
+              type='text'
+              id='email'
+              className='form-input'
+              {...register('email')}
+            />
+            {errors.email && (
+              <p className='input-error'>Niepoprawny adres e-mail.</p>
+            )}
+          </div>
+          <div className='field-wrapper'>
+            <label htmlFor='message'>Wiadomość</label>
+            <textarea
+              id='message'
+              className='form-input h-50 resize-none'
+              {...register('message')}></textarea>
+            {errors.message && (
+              <p className='input-error'>Wpisz pomiędzy 20 a 400 znaków.</p>
+            )}
+          </div>
+          <button className='bg-yellow hover:bg-yellow-dim transition-bg h-14 w-30 cursor-pointer text-xl font-medium duration-200'>
+            WYŚLIJ
+          </button>
+        </form>
       </div>
     </section>
   );
