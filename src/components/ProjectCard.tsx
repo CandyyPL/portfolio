54f9e0544133e2b5d53d1a844dd5d541654bca00
CandyyPL <aplay58.yt@gmail.com>
2026-03-05 17:55:34 +0100

    tweak project card styling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,13 +13,15 @@ export default function ProjectCard({ project }: Props) {
           alt='thumbnail'
           className='block w-full rounded-t-md'
         />
-        <div className='flex min-h-67.5 flex-col justify-center gap-4 p-4'>
-          <h1 className='text-light w-full text-2xl font-medium'>
-            {project.projectname}
-          </h1>
-          <p className='text-light-dim line-clamp-4 text-lg'>
-            {project.projectdesc}
-          </p>
+        <div className='flex min-h-67.5 flex-col justify-between gap-4 p-4'>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-light w-full text-2xl font-medium'>
+              {project.projectname}
+            </h1>
+            <p className='text-light-dim line-clamp-4 text-lg'>
+              {project.projectdesc}
+            </p>
+          </div>
           <button className='bg-yellow hover:bg-yellow-dim transition-bg text-dark w-full cursor-pointer px-6 py-2 text-lg duration-200'>
             Czytaj więcej
           </button>
