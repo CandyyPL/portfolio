943cc170b9ca3c915dcf26e537812df41bdf304d
CandyyPL <aplay58.yt@gmail.com>
2026-03-05 17:02:48 +0100

    add mockup query for projects with loading & error handling

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,21 +1,23 @@
-export default function ProjectCard() {
+import type { ProjectData } from '@/sections/Projects.tsx';
+
+type Props = {
+  project: ProjectData;
+};
+
+export default function ProjectCard({ project }: Props) {
   return (
     <li>
       <article className='project-card'>
         <img
-          src='https://placehold.co/1280x720'
+          src={project.thumbnailUrl}
           alt='thumbnail'
           className='block w-full rounded-t-md'
         />
         <div className='flex min-h-67.5 flex-col justify-center gap-5 px-4'>
           <h1 className='text-light w-full text-2xl font-medium'>
-            Example name
+            {project.title}
           </h1>
-          <p className='text-light-dim line-clamp-4 text-lg'>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
-            corporis doloremque explicabo fuga illo natus officiis provident
-            velit?
-          </p>
+          <p className='text-light-dim line-clamp-4 text-lg'>{project.desc}</p>
           <button className='bg-yellow hover:bg-yellow-dim transition-bg text-dark w-full cursor-pointer px-6 py-2 text-lg duration-200'>
             Czytaj więcej
           </button>
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,8 +1,53 @@
 import { SECTIONS } from '@/lib/constants.ts';
 import ProjectCard from '@/components/ProjectCard.tsx';
 import githubIcon from '@/assets/icons/github-mark.png';
+import warningIcon from '@/assets/icons/warning-white.png';
+import { queryOptions, useQuery } from '@tanstack/react-query';
+import { TailSpin } from 'react-loader-spinner';
+
+export type ProjectData = {
+  id: string;
+  title: string;
+  desc: string;
+  thumbnailUrl: string;
+};
+
+type ApiResponse = {
+  data: ProjectData[] | null;
+  error?: Error | null;
+};
+
+const projects: ProjectData[] = [
+  {
+    id: '1',
+    title: 'Example',
+    desc: 'Desc',
+    thumbnailUrl: 'https://placehold.co/1280x720',
+  },
+];
+
+const getProjects = async (fail?: boolean) =>
+  new Promise<ApiResponse>((resolve) => {
+    setTimeout(() => {
+      if (fail) {
+        resolve({ error: new Error('Error'), data: null });
+      } else {
+        resolve({ data: projects });
+      }
+    }, 3000);
+  });
+
+const getProjectsQueryOptions = () =>
+  queryOptions({
+    queryKey: ['projects'],
+    queryFn: () => getProjects(false),
+  });
 
 export default function Projects() {
+  const { data, isPending } = useQuery(getProjectsQueryOptions());
+
+  const projects = data?.data ?? null;
+
   return (
     <section
       id={SECTIONS.PROJECTS}
@@ -11,9 +56,29 @@ export default function Projects() {
         A oto kilka moich <span className='text-yellow'>projektów</span>..
       </h1>
       <ul className='flex flex-col items-center gap-8 md:w-full md:flex-row md:flex-wrap md:justify-center'>
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {isPending ? (
+          <div className='flex min-h-67.5 flex-col items-center justify-center gap-4'>
+            <TailSpin
+              width='50px'
+              height='50px'
+              color='#C1C3C2'
+            />
+            <p className='text-light text-lg'>Ładowanie</p>
+          </div>
+        ) : projects ? (
+          projects.map((project) => <ProjectCard project={project} />)
+        ) : (
+          <div className='flex min-h-67.5 flex-col items-center justify-center gap-4'>
+            <img
+              src={warningIcon}
+              alt='error'
+              className='size-14 opacity-90'
+            />
+            <p className='text-light text-center text-lg'>
+              Wystąpił błąd. <br /> Przepraszam za utrudnienia.
+            </p>
+          </div>
+        )}
       </ul>
       <button className='bg-yellow text-dark hover:bg-yellow-dim transition-bg px-4 py-2 text-xl duration-200'>
         <a
