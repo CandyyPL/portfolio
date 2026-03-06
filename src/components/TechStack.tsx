30a322f2c0de232dcde3c05f7ff04b18ee1f70c0
CandyyPL <aplay58.yt@gmail.com>
2026-03-06 15:24:26 +0100

    import extracted icons

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,42 +1,4 @@
-import tsIcon from '@/assets/icons/typescript-plain.svg';
-import reactIcon from '@/assets/icons/react-original.svg';
-import zustandIcon from '@/assets/icons/zustand-plain.svg';
-import sassIcon from '@/assets/icons/sass-original.svg';
-import tailwindIcon from '@/assets/icons/tailwindcss-original.svg';
-import supabaseIcon from '@/assets/icons/supabase-plain.svg';
-
-const ICONS = [
-  {
-    name: 'ts',
-    icon: tsIcon,
-    displayName: 'TypeScript',
-  },
-  {
-    name: 'react',
-    icon: reactIcon,
-    displayName: 'React',
-  },
-  {
-    name: 'zustand',
-    icon: zustandIcon,
-    displayName: 'Zustand',
-  },
-  {
-    name: 'sass',
-    icon: sassIcon,
-    displayName: 'Sass',
-  },
-  {
-    name: 'tailwind',
-    icon: tailwindIcon,
-    displayName: 'Tailwind CSS',
-  },
-  {
-    name: 'supabase',
-    icon: supabaseIcon,
-    displayName: 'Supabase',
-  },
-];
+import { ICONS } from '@/lib/icons.ts';
 
 export default function TechStack() {
   return (
