31b050888227dc8b06fe228d1a4f8ff8c1884d5f
CandyyPL <aplay58.yt@gmail.com>
2026-03-06 15:23:25 +0100

    refactor field names;

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -9,20 +9,20 @@ export default function ProjectCard({ project }: Props) {
     <li>
       <article className='project-card'>
         <img
-          src={project.projectthumbnail.url}
+          src={project.thumbnail.url}
           alt='thumbnail'
           className='block w-full rounded-t-md'
         />
         <div className='flex min-h-67.5 flex-col justify-between gap-4 p-4'>
           <div className='flex flex-col gap-2'>
             <h1 className='text-light w-full text-2xl font-medium'>
-              {project.projectname}
+              {project.name}
             </h1>
             <p className='text-light-dim line-clamp-4 text-lg'>
-              {project.projectdesc}
+              {project.description}
             </p>
           </div>
-          <a href={`/project/${project.projectslug}`}>
+          <a href={`/project/${project.slug}`}>
             <button className='bg-yellow hover:bg-yellow-dim transition-bg text-dark w-full cursor-pointer px-6 py-2 text-lg duration-200'>
               Czytaj więcej
             </button>
