f26736e3b4ee7f2aa8aa55857256f5265eac7e82
CandyyPL <aplay58.yt@gmail.com>
2026-03-06 16:00:07 +0100

    made project wrapper page

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,15 +1,57 @@
 import Background from '@/components/Background.tsx';
 import Topbar from '@/components/Topbar.tsx';
+import { useParams } from 'react-router';
+import { useQuery } from '@tanstack/react-query';
+import { getProjectsQueryOptions } from '@/lib/getProjects.ts';
+import { TailSpin } from 'react-loader-spinner';
+import { Activity } from 'react';
+import warningIcon from '@/assets/icons/warning-white.png';
 import SingleProject from '@/components/SingleProject.tsx';
+import Footer from '@/components/Footer.tsx';
 
 export default function ProjectPage() {
+  const { slug } = useParams();
+
+  const { data, error, isPending } = useQuery(getProjectsQueryOptions());
+
+  const project = data?.find((project) => project.slug === slug) ?? null;
+
   return (
     <>
       <Background />
-      <Topbar />
-      <main className='main-content'>
-        <SingleProject />
+      <main className='main-content items-center p-8'>
+        <section className='flex w-full max-w-300 flex-col items-center gap-4'>
+          <Activity mode={isPending && !error ? 'visible' : 'hidden'}>
+            <div className='flex h-full w-full flex-col items-center justify-center gap-4'>
+              <TailSpin
+                width='50px'
+                height='50px'
+                color='#C1C3C2'
+              />
+              <p className='text-light text-lg'>Ładowanie</p>
+            </div>
+          </Activity>
+          <Activity mode={error ? 'visible' : 'hidden'}>
+            <div className='flex h-full w-full flex-col items-center justify-center gap-4'>
+              <img
+                src={warningIcon}
+                alt='error'
+                className='size-14 opacity-90'
+              />
+              <p className='text-light text-center text-lg'>
+                Wystąpił błąd. <br /> Przepraszam za utrudnienia.
+              </p>
+              <a href='/'>
+                <button className='bg-yellow hover:bg-yellow-dim transition-bg text-dark w-full cursor-pointer px-6 py-2 text-lg duration-200'>
+                  Strona główna
+                </button>
+              </a>
+            </div>
+          </Activity>
+          {project && <SingleProject project={project} />}
+        </section>
       </main>
+      <Footer />
     </>
   );
 }
