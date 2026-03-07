731988d1a40d5ec83e6b1877637c97fd7100c6e7
CandyyPL <aplay58.yt@gmail.com>
2026-03-07 05:53:35 +0100

    add todo

--- src/components/SingleProject.tsx
+++ src/components/SingleProject.tsx
@@ -18,7 +18,7 @@ export default function SingleProject({ project }: Props) {
         </h1>
         <div className='flex w-full justify-start md:justify-end'>
           <LinkButton
-            url='/'
+            url='/' // TODO: go back to /#projects
             buttonStyle='link-button'
             blank={false}>
             Powrót
