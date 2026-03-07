f8a084a8a8157130ea8eb79e699953d46f2107e5
CandyyPL <aplay58.yt@gmail.com>
2026-03-07 21:48:31 +0100

    remove config for sub domain

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Background />
-      <Router basename='/portfolio/'>
+      <Router>
         <Routes>
           <Route
             path='/'
